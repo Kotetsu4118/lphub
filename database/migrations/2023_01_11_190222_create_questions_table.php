40380c927f847fbaa1d4444e135ee8c0ddc3aa85
Kotetsu4118 <skyhigh.1848@gmail.com>
2023-01-16 13:34:43 +0900

    profiles_bases

---
+++ database/migrations/2023_01_11_190222_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 20);
+            $table->string('body', 2000);
+            $table->string('answer', 2000);
+            $table->foreignId('language_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamp("created_at")->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp("updated_at")->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->softDeletes("deleted_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
---
+++ resources/views/NULL.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <title>NULL</title>
+</head>
+
+<body>
+    {{$type}}
+    
+</body>
+    
+</html>
